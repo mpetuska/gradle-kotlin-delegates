04faac14d8295631294e4f2de6f86a30ad73578c
Martynas Petuška <martynas@petuska.dev>
2022-01-24 21:25:43 +0000

    GEnvironmentDelegate

--- src/main/kotlin/GEnvironmentDelegate.kt
+++ src/main/kotlin/GEnvironmentDelegate.kt
@@ -1,30 +1,31 @@
 package dev.petuska.gradle.kotlin.delegates
 
-import org.gradle.api.Project
 import kotlin.reflect.KProperty
 
 /**
  * TODO
  */
 public class GEnvironmentDelegate<T>(
-  private val project: Project,
   prefix: String? = null,
+  private val getEnv: (String) -> String? = System::getenv,
   private val converter: (String) -> T,
 ) : GLazy<T?> {
-  private val prefix = prefix?.lowercase()?.replace(separators, ".")
-    ?.removeSurrounding(".")
-  private val upperPrefix = prefix?.uppercase()?.replace(separators, "_")
+  private val lowerPrefix = prefix?.replace(separators, ".")?.removeSuffix(".")
+    ?.takeIf(String::isNotBlank)?.lowercase()
+
+  private val upperPrefix = lowerPrefix?.uppercase()
+    ?.replace(separators, "_")
+
   override fun getValue(thisRef: Any?, property: KProperty<*>): T? {
-    val value = project.findProperty(upperPrefix?.plus("_${property.name.uppercase()}") ?: property.name.uppercase())
-      ?: project.findProperty(prefix?.plus(".${property.name}") ?: property.name)
-    return value?.toString()?.let(converter)
+    val value = getEnv(upperPrefix?.plus("_${property.name.uppercase()}") ?: property.name.uppercase())
+      ?: getEnv(lowerPrefix?.plus(".${property.name}") ?: property.name)
+    return value?.let(converter)
   }
 
   public companion object {
-    private val separators = Regex("[.\\- _]")
+    internal val separators = Regex("[.\\- _]")
     public operator fun invoke(
-      project: Project,
       prefix: String? = null
-    ): GEnvironmentDelegate<String> = GEnvironmentDelegate(project, prefix) { it }
+    ): GEnvironmentDelegate<String> = GEnvironmentDelegate(prefix) { it }
   }
 }
